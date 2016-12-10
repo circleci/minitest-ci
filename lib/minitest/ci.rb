6cbdab5a9c1bc662e35cd9d2e8251a677389d078
Andrew Prasetyo <zeroryu@users.noreply.github.com>
2016-12-09 19:24:49 -0800

    more url friendly test report filename
    Allows for better compatibility with circle ci artifact uploads.
    
    ParentClass::ChildClass::ExampleController will generate parent_class/child_class/example_controller-TEST.xml

--- lib/minitest/ci.rb
+++ lib/minitest/ci.rb
@@ -54,7 +54,7 @@ module Minitest
 
       Dir.chdir report_dir do
         results.each do |name, resultz|
-          File.open "TEST-#{CGI.escape(name.to_s)}.xml"[0, 255], "w" do |f|
+          File.open "#{CGI.escape(name.to_s.underscore)}-TEST.xml"[0, 255], "w" do |f|
             f.puts generate_results name, resultz
           end
         end
